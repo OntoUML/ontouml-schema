24dd647c3b20b3240117f58f488fce7a6cb7a99b
Claudenir M. Fonseca <claudenirmf@gmail.com>
2019-08-22 17:44:11 +0200

    Refactoring

--- src/validator.js
+++ src/validator.js
@@ -2,7 +2,7 @@ const Ajv = require('ajv');
 const fs = require('fs');
 
 let schemaFiles = [
-  "../../schemas/ontouml.schema.json"
+  "schemas/ontouml.schema.json"
 ];
 
 let schemas = schemaFiles.map( file => {
@@ -12,7 +12,7 @@ let schemas = schemaFiles.map( file => {
 let ajv = new Ajv({schemas: schemas});
 let validate = ajv.getSchema("https://ontouml.org/OntoUMLSchema/v1/Model");
 
-let object = JSON.parse(fs.readFileSync("../../examples/m1.example.json", 'utf8'))
+let object = JSON.parse(fs.readFileSync("examples/m1.example.json", 'utf8'))
 let isValid = validate(object);
 
 if(isValid)
@@ -21,3 +21,5 @@ else {
   console.log("Model M1 is INVALID!");
   console.log(validate.errors);
 }
+
+exports.schema = schemas[0];
