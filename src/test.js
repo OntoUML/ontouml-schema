969d3c6b301bec9976b711daacdabea42fe20089
Claudenir M. Fonseca <claudenirmf@gmail.com>
2019-08-25 23:12:19 +0200

    Experimenting with module exports

---
+++ src/test.js
@@ -0,0 +1,6 @@
+const OntoUMLSchema = require('./validator')
+
+//var instance = new OntoUMLSchema();
+
+console.log(OntoUMLSchema);
+console.log(OntoUMLSchema.getSchema());
--- src/validator.js
+++ src/validator.js
@@ -1,25 +1,36 @@
 const Ajv = require('ajv');
 const fs = require('fs');
+const ds = require("./../schemas/ontouml.schema.json");
 
-let schemaFiles = [
-  "schemas/ontouml.schema.json"
-];
+// let schemaFiles = [
+//   "schemas/ontouml.schema.json"
+// ];
+//
+// let schemas = schemaFiles.map( file => {
+//   return JSON.parse(fs.readFileSync(file, 'utf8'))
+// })
+//
+// let ajv = new Ajv({schemas: schemas});
+// let validate = ajv.getSchema("https://ontouml.org/OntoUMLSchema/v1/Model");
+//
+// let object = JSON.parse(fs.readFileSync("examples/m1.example.json", 'utf8'))
+// let isValid = validate(object);
+//
+// if(isValid)
+//   console.log("Model M1 is VALID!");
+// else {
+//   console.log("Model M1 is INVALID!");
+//   console.log(validate.errors);
+// }
 
-let schemas = schemaFiles.map( file => {
-  return JSON.parse(fs.readFileSync(file, 'utf8'))
-})
-
-let ajv = new Ajv({schemas: schemas});
-let validate = ajv.getSchema("https://ontouml.org/OntoUMLSchema/v1/Model");
-
-let object = JSON.parse(fs.readFileSync("examples/m1.example.json", 'utf8'))
-let isValid = validate(object);
-
-if(isValid)
-  console.log("Model M1 is VALID!");
-else {
-  console.log("Model M1 is INVALID!");
-  console.log(validate.errors);
+module.exports = {
+  getSchema : function() {
+    return ds;
+    //return JSON.parse(fs.readFileSync("./../schemas/ontouml.schema.json", 'utf8'))
+  },
+  getValidator: function() {
+    var schema = this.getSchema();
+    var asd = new Ajv({schemas: [schema]});
+    return asd.getSchema("https://ontouml.org/OntoUMLSchema/v1/Model");
+  }
 }
-
-exports.schema = schemas[0];
