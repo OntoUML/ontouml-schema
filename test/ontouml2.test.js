d146823f6ffd66c056aeb120753275f8f562231a
Claudenir M. Fonseca <claudenirmf@gmail.com>
2020-01-28 16:13:12 +0100

    BugFix Missing propertyType
    - BugFix Missing propertyType
    - Moved test models from /examples to /test/test_models
    - Updated README
    - Updated LICENSE
    - Version set to 0.1.0

--- test/ontouml2.test.js
+++ test/ontouml2.test.js
@@ -13,61 +13,61 @@ function testOntoUML2Model(modelPath) {
 }
 
 test('Model: minimal model', () => {
-    testOntoUML2Model('./../examples/model.minimum.json')
+    testOntoUML2Model('./test_models/model.minimum.json')
 });
 
 test('Model: all fields', () => {
-    testOntoUML2Model('./../examples/model.all.fields.json')
+    testOntoUML2Model('./test_models/model.all.fields.json')
 });
 
 test('Package: minimal package', () => {
-    testOntoUML2Model('./../examples/package.all.fields.json')
+    testOntoUML2Model('./test_models/package.all.fields.json')
 });
 
 test('Package: all fields', () => {
-    testOntoUML2Model('./../examples/package.all.fields.json')
+    testOntoUML2Model('./test_models/package.all.fields.json')
 });
 
 test('Class: minimal class', () => {
-    testOntoUML2Model('./../examples/class.minimum.json')
+    testOntoUML2Model('./test_models/class.minimum.json')
 });
 
 test('Class: all fields', () => {
-    testOntoUML2Model('./../examples/class.all.fields.json')
+    testOntoUML2Model('./test_models/class.all.fields.json')
 });
 
 test('Relation: minimal relation', () => {
-    testOntoUML2Model('./../examples/relation.minimum.json')
+    testOntoUML2Model('./test_models/relation.minimum.json')
 });
 
 test('Relation: all fields', () => {
-    testOntoUML2Model('./../examples/relation.all.fields.json')
+    testOntoUML2Model('./test_models/relation.all.fields.json')
 });
 
 test('Property: minimal property', () => {
-    testOntoUML2Model('./../examples/property.minimum.json')
+    testOntoUML2Model('./test_models/property.minimum.json')
 });
 
 test('Property: all fields', () => {
-    testOntoUML2Model('./../examples/property.all.fields.json')
+    testOntoUML2Model('./test_models/property.all.fields.json')
 });
 
 test('Generalization: minimal generalization', () => {
-    testOntoUML2Model('./../examples/generalization.minimum.json')
+    testOntoUML2Model('./test_models/generalization.minimum.json')
 });
 
 test('Generalization: all fields', () => {
-    testOntoUML2Model('./../examples/generalization.all.fields.json')
+    testOntoUML2Model('./test_models/generalization.all.fields.json')
 });
 
 test('GeneralizationSet: minimal generalization set', () => {
-    testOntoUML2Model('./../examples/generalizationset.minimum.json')
+    testOntoUML2Model('./test_models/generalizationset.minimum.json')
 });
 
 test('GeneralizationSet: all fields', () => {
-    testOntoUML2Model('./../examples/generalizationset.all.fields.json')
+    testOntoUML2Model('./test_models/generalizationset.all.fields.json')
 });
 
 test('propertyAssignments: all options', () => {
-    testOntoUML2Model('./../examples/propertyassignments.all.options.json')
+    testOntoUML2Model('./test_models/propertyassignments.all.options.json')
 });
